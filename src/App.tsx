553cfa9130cd66c6e9e226b0dd74aa6c79510832
DavidAntonioO <david-antonio.olmos@atos.net>
2022-03-04 10:16:56 -0800

    Lifecycle-Error

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { ErrorInfo } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Button} from './components/Button';
-function App() {
-  return (
-    <>
-       <h1>My App</h1>
-       <Button type='primary'>Test</Button>
-       <Button>Primary</Button>
-    </>
-  );
+import FirstComponent from './components/FirstComponent'
+import { FirstComponentProps } from './components/FirstComponent/interface';
+import ErrorComponent from './components/ErrorComponent';
+
+   interface AddProps {}
+
+   interface AppState{
+      hasError: boolean;
+  }
+
+class App extends React.Component<AddProps, AppState> {
+
+      constructor(props: AddProps){
+        super(props);
+
+        this.state ={
+            hasError: false
+        }
+    }
+
+      static getDerivedStateFromError(error: Error){
+        console.log('getDerivedStateFromError:', error);
+
+        return {
+            hasError: true
+        };
+    }
+
+    componentDidCatch(error: Error, info: ErrorInfo){
+        console.log('componentDidCatch:', error);
+        console.log('componentDidCatch:', info);
+
+        this.setState({
+          hasError: true
+        })
+    }
+    render(){
+      return (
+        <>
+          <h1>My App</h1>
+          {this.state.hasError ? <ErrorComponent/> : <FirstComponent/>}
+        </>
+      )
+    }
 }
 
 export default App;
