9224e070331929ecc433ebe142c872e2b92db4be
DavidAntonioO <david-antonio.olmos@atos.net>
2022-03-22 17:59:35 -0700

    Children Clone Element

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Button} from './components/Button';
-function App() {
-  return (
-    <>
-       <h1>My App</h1>
-       <Button type='primary'>Test</Button>
-       <Button>Primary</Button>
-    </>
+import HomePage from './component/HomePage';
+
+function App(){
+  return(
+    <div>
+      <HomePage />
+    </div>
   );
 }
 
